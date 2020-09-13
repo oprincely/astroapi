ae6f7329dacd12c6b1e0379f99d1519a64cad59c
Oprincely <oprincely@gmail.com>
2020-09-13 17:45:03 +0100

    v0

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from typing import Optional
 
 from fastapi import FastAPI
 
+from fastapi.middleware.cors import CORSMiddleware
 #from fastapi.staticfiles import StaticFiles
 #from fastapi.templating import Jinja2Templates
 
@@ -9,15 +10,25 @@ app = FastAPI()
 
 #app.mount("/static", StaticFiles(directory="static"), name="static")
 #template = Jinja2Templates(directory="./templates")
+origins = [
+    "http://wec.org.ng",
+]
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
-def read_root():
+async def read_root():
     return {"Hello": "World"}
 
 
 @app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
+async def read_item(item_id: int, q: Optional[str] = None):
     return {"item_id": item_id, "q": q}
 
 ###############################
